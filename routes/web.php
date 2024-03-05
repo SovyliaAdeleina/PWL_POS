671ec1ddfd3e1e4c7e11ee7c1b8bf20c56c587d6
SovyliaAdeleina <sovylia27@gmail.com>
2024-03-05 15:32:33 +0700

    praktikum 6

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controller\KategoriController;
 use App\Http\Controllers\KategoriController as ControllersKategoriController;
 use App\Http\Controllers\LevelController;
+use App\Http\Controllers\UserController; 
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -11,3 +12,4 @@ Route::get('/', function () {
 
 Route::get('/level', [LevelController::class, 'index']);
 Route::get('/kategori', [ControllersKategoriController::class, 'index']);
+Route::get('/user', [UserController::class, 'index']);
