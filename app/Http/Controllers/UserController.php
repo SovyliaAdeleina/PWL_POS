52c8f0117adddadd859ba17400efc1263345c681
SovyliaAdeleina <sovylia27@gmail.com>
2024-03-11 21:15:59 +0700

    praktikum 1-jobsheet 4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,11 +10,20 @@ class UserController extends Controller
 {
     public function index()
     {
-        // tambah data user dengan Eloquent Model
         $data = [
-            'nama' => 'Pelanggan Pertama',
+            
+            'level_id' => 2,
+            'username' => 'manager_tiga',
+            'nama' => 'Manager 3',
+            'password' => Hash::make('12345')
         ];
-        UserModel::where('username', 'customer-1')->update($data); //update data user
+        UserModel::create($data);
+
+        // tambah data user dengan Eloquent Model
+        // $data = [
+        //     'nama' => 'Pelanggan Pertama',
+        // ];
+        // UserModel::where('username', 'customer-1')->update($data); //update data user
 
         // $data = [
         //     'username' => 'customer-1',
@@ -24,8 +33,8 @@ class UserController extends Controller
         // ];
         // UserModel::insert($data); // tambahkan data ke tabel m_user
 
-        // coba akses model UserModel
-        $user = UserModel::all(); // ambil semua data dari tabel m_user
+        
+        $user = UserModel::all(); 
         return view('user', ['data' => $user]);
     }
 }
