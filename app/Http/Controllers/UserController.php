264447837fc7eab53861a175af254f3e8def3c69
SovyliaAdeleina <sovylia27@gmail.com>
2024-03-14 21:15:33 +0700

    praktikum 2.1 No.10

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -39,7 +39,19 @@ class UserController extends Controller
         // $user = UserModel::where('level_id', 1)->first(); 
         // return view('user',  ['data' => $user]);
 
-        $user = UserModel::firstWhere('level_id', 1); 
+        // $user = UserModel::firstWhere('level_id', 1); 
+        // return view('user',  ['data' => $user]);
+
+        // $user = UserModel::findOr(1, ['username', 'nama'], function() {
+        //     abort(404);
+        // }); 
+
+        // return view('user',  ['data' => $user]);
+
+        $user = UserModel::findOr(20, ['username', 'nama'], function() {
+            abort(404);
+        }); 
+        
         return view('user',  ['data' => $user]);
     }
 }
