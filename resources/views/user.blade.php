732ed546a2c70077dc374696eb75fcd328ce7791
SovyliaAdeleina <sovylia27@gmail.com>
2024-03-15 23:26:03 +0700

    praktikum 2.7

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -11,7 +11,9 @@
                 <td>ID</td>
                  <td>Username</td> 
                  <td>Nama</td>
-                <td>ID Level Pengguna</td> 
+                <td>ID Level Pengguna</td>
+                <td>Kode Level</td>
+                <td>Nama Level</td> 
                 <td>Aksi</td>
             </tr>
             @foreach ($data as $d)
@@ -21,10 +23,11 @@
                 <td>{{ $d->username}}</td>
                 <td>{{ $d->nama}}</td>
                 <td>{{ $d->level_id}}</td>
+                <td>{{ $d->level->level_kode }}</td>
+                <td>{{ $d->level->level_nama }}</td>
                 <td><a href="user/ubah/{{ $d->user_id}}">Ubah</a> | <a href="user/hapus/{{ $d->user_id }}">Hapus></a></td>
-                {{-- <td>{{$data}}</td> --}}
             </tr>
             @endforeach
         </table>
-    </body>
+   </body>
 </html>
