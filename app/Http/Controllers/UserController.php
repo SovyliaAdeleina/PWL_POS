2002ab39c4866d4f186a7cb217eeb5ddfdf35f21
SovyliaAdeleina <sovylia27@gmail.com>
2024-04-02 06:09:28 +0700

    Praktikum 4, Jobsheet 7

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -30,7 +30,9 @@ class UserController extends Controller
 
         $activeMenu = 'user'; // set menu yang sedang aktif
 
-        return view('user.index', ['breadcrumb' => $breadcrumb, 'page' => $page, 'activeMenu' => $activeMenu]);
+        $level = LevelModel::all();
+
+        return view('user.index', ['breadcrumb' => $breadcrumb, 'page' => $page, 'level' => $level, 'activeMenu' => $activeMenu]);
     }
     // Ambil data user dalam bentuk json untuk datatables
     public function list(Request $request)
@@ -38,24 +40,25 @@ class UserController extends Controller
         $user =User::select('user_id', 'username', 'nama', 'level_id')
         ->with('level');
         
+        // Praktikum 4, Jobsheet 7
+        if ($request->level_id) {
+            $user->where('level_id', $request->level_id);
+        }
+
         return DataTables::of($user)
-        
-        ->addIndexColumn() // menambahkan kolom index / no urut (default nama kolom: DT_RowIndex)
-        ->addColumn('aksi', function ($user) { // menambahkan kolom aksi
-        $btn = '<a href="'.url('/user/' . $user->user_id).'" class="btn btn-info btn-sm">Detail</a> ';
-        $btn .= '<a href="'.url('/user/' . $user->user_id . '/edit').'" 
-class="btn btn-warning btn-sm">Edit</a> ';
-$btn .= '<form class="d-inline-block" method="POST" action="'. 
-url('/user/'.$user->user_id).'">'
-            . csrf_field() . method_field('DELETE') . 
-'<button type="submit" class="btn btn-danger btn-sm" 
-onclick="return confirm(\'Apakah Anda yakin menghapus data 
-ini?\');">Hapus</button></form>'; 
-            return $btn;
-        })
-        ->rawColumns(['aksi']) // memberitahu bahwa kolom aksi adalah html
-        ->make(true);
-}
+            ->addIndexColumn() // menambahkan kolom index / no urut (default nama kolom: DT_RowIndex)
+            ->addColumn('aksi', function ($user) { // menambahkan kolom aksi
+                $btn = '<a href="' . url('/user/' . $user->user_id) . '" class="btn btn-info btn-sm">Detail</a> ';
+                $btn .= '<a href="' . url('/user/' . $user->user_id . '/edit') . '" class="btn btn-warning btn-sm">Edit</a> ';
+                $btn .= '<form class="d-inline-block" method="POST" action="' . url('/user/' . $user->user_id) . '">'
+                    . csrf_field() . method_field('DELETE') .
+                    '<button type="submit" class="btn btn-danger btn-sm" onclick="return confirm(\'Apakah Anda yakit menghapus data ini?\');">Hapus</button></form>';
+                return $btn;
+            })
+            ->rawColumns(['aksi']) // memberitahu bahwa kolom aksi adalah html
+            ->make(true);
+    }
+
 // Praktikum 3, Jobsheet 7
 public function create()
 {
--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -16,20 +16,43 @@
         @if (session('error'))
             <div class="alert alert-danger">{{ session('error') }}</div>
         @endif
-          
-            {{-- <table class="table table-bordered table-striped table-hover table-sm"
-            id="table_user">
-            <thead>
-            <tr><th>ID</th><th>Username</th><th>Nama</th><th>Level Pengguna</th><th>Aksi</th></tr>
-            </thead>
-            </table> --}}
+        {{-- // Praktikum 4, Jobsheet 7, No 2 --}}
+        <div class="row">
+            <div class="col-ms-12">
+                <div class="form-group row">
+                    <label class="col-4 control-label col-form-label">Filter :</label>
+                    <div class="col-8">
+                        <select name="level_id" id="level_id" class="form-control" required>
+                            <option value="">- Semua -</option>
+                            @foreach ($level as $item)
+                                <option value="{{ $item->level_id }}">{{ $item->level_nama }}</option>
+                            @endforeach
+                        </select>
+                        <small class="form-text text-muted">Level Pengguna</small>
+                    </div>
+                </div>
+            </div>
         </div>
+    
+        <table class="table table-bordered table-striped table-hover table-sm" id="table_user">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Username</th>
+                    <th>Nama</th>
+                    <th>Level Pengguna</th>
+                    <th>Aksi</th>
+                </tr>
+            </thead>
+        </table>
     </div>
+</div>
 @endsection
 
 @push('css')
 @endpush
 @push('js')
+{{-- Praktikum 4, Jobsheet 4, No 3 --}}
     <script>
         $(document).ready(function() {
             var dataUser = $('#table_user').DataTable({
@@ -38,6 +61,8 @@
                     "url": "{{ url('user/list') }}",
                     "dataType": "json",
                     "type": "POST"
+                    "data": function(d) {
+                        d.level_id = $('#level_id').val();
                 },
 columns: [
     {
@@ -78,5 +103,12 @@ columns: [
                 }
             ]
         });
+
+        // Praktikum 4, Jobsheet 7, No 4
+        $('#level_id').on('change', function() {
+                dataUser.ajax.reload();
+        });
+
+    });
         </script>
 @endpush           
