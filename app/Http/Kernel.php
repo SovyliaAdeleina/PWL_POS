258fa3d6256d530881f8c0eef9101f1a27a4e056
SovyliaAdeleina <sovylia27@gmail.com>
2024-04-22 22:15:59 +0700

    JS 9 - Praktikum No 4

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,5 +64,7 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        // JS 9 Praktikum No 4
+        'cek_login' => \App\Http\Middleware\Cek_login::class,
     ];
 }
