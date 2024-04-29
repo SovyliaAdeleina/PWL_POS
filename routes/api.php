5c138f78619261f63f684be09e5c78cc62a68f45
SovyliaAdeleina <sovylia27@gmail.com>
2024-04-29 23:58:29 +0700

    JS 10 - Praktikum 3 & 4

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\RegisterController;
+use App\Http\Controllers\LevelController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,9 +19,18 @@ use Illuminate\Support\Facades\Route;
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
 
-// Jobsheet 10 - Praktikum 1, No 10
+// Jobsheet 10 
 Route::post('/register', App\Http\Controllers\Api\RegisterController::class)->name('register');
 Route::post('/login', App\Http\Controllers\Api\LoginController::class)->name('login');
+Route::post('/logout', App\Http\Controllers\Api\LogoutController::class)->name('logout');
+
+// Jobsheet 10 - Praktikum 4
+Route::get('levels', [LevelController::class, 'index']);
+Route::post("levels",[LevelController::class,'store']);
+Route::get("levels/{level}",[LevelController::class,'show']);
+Route::put("levels/{level}",[LevelController::class,'update']);
+Route::delete("levels/{level}",[LevelController::class,'destroy']);
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
